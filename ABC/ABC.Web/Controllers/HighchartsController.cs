8935ed3258bedfcc2d76ed8accc672abd65886a0
LiuHaixin <liuhaixin2016@outlook.com>
2017-07-04 18:11:54 +0800

    highcharts

---
+++ ABC/ABC.Web/Controllers/HighchartsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ABC.Web.Controllers
+{
+    public class HighchartsController : Controller
+    {
+        // GET: Highcharts
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Pie()
+        {
+            return View();
+        }
+    }
+}
