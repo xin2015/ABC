fe896606b0edd30a48356969901b0db4c88ae4af
LiuHaixin <liuhaixin2016@outlook.com>
2017-08-01 23:49:29 +0800

    bmap

---
+++ ABC/ABC.Web/Controllers/EChartsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ABC.Web.Controllers
+{
+    public class EChartsController : Controller
+    {
+        // GET: ECharts
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
