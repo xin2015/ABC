f9dbbac4ba3b52a92b8b646ba59dcb627765ab04
LiuHaixin <liuhaixin2016@outlook.com>
2017-08-17 02:40:36 +0800

    NPOI

---
+++ ABC/ABC.BLL/NPOI/WordHelper.cs
@@ -0,0 +1,186 @@
+﻿using ABC.BLL.Tabulation;
+using NPOI.OpenXmlFormats.Wordprocessing;
+using NPOI.XWPF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABC.BLL.NPOI
+{
+    public class WordHelper
+    {
+        private static XWPFDocument GetDocument(Table table)
+        {
+            int headRowCount = table.Thead.cellStatus.Count, bodyRowCount = table.Tbody.cellStatus.Count, footRowCount = table.Tfoot.cellStatus.Count;
+            int colCount = 0, rowCount = headRowCount + bodyRowCount + footRowCount;
+            foreach (var status in table.Thead.cellStatus)
+            {
+                if (status.Count > colCount) colCount = status.Count;
+            }
+            foreach (var status in table.Tbody.cellStatus)
+            {
+                if (status.Count > colCount) colCount = status.Count;
+            }
+            foreach (var status in table.Tfoot.cellStatus)
+            {
+                if (status.Count > colCount) colCount = status.Count;
+            }
+            XWPFDocument doc = new XWPFDocument();
+            XWPFTable xt = doc.CreateTable(rowCount, colCount);
+            #region 填充thead
+            foreach (TRow tr in table.Thead)
+            {
+                XWPFTableRow xtr = xt.GetRow(tr.Index);
+                foreach (TCell th in tr)
+                {
+                    XWPFTableCell cell = xtr.GetCell(th.Index);
+                    cell.SetText(th.Value);
+                    if (th.Colspan > 1)
+                    {
+                        CT_Tc cttc = cell.GetCTTc();
+                        CT_TcPr tcpr = cttc.AddNewTcPr();
+                        tcpr.gridSpan = new CT_DecimalNumber();
+                        tcpr.gridSpan.val = th.Colspan.ToString();
+                        if (th.Rowspan > 1)
+                        {
+                            tcpr.AddNewVMerge().val = ST_Merge.restart;
+                            for (int i = 1; i < th.Rowspan; i++)
+                            {
+                                XWPFTableRow xtri = xt.GetRow(tr.Index + i);
+                                XWPFTableCell celli = xtri.GetCell(th.Index);
+                                CT_Tc cttci = celli.GetCTTc();
+                                CT_TcPr tcpri = cttci.AddNewTcPr();
+                                tcpri.gridSpan = new CT_DecimalNumber();
+                                tcpri.gridSpan.val = th.Colspan.ToString();
+                                tcpri.AddNewVMerge().val = ST_Merge.@continue;
+                            }
+                        }
+                    }
+                    else if (th.Rowspan > 1)
+                    {
+                        CT_Tc cttc = cell.GetCTTc();
+                        CT_TcPr tcpr = cttc.AddNewTcPr();
+                        tcpr.AddNewVMerge().val = ST_Merge.restart;
+                        for (int i = 1; i < th.Rowspan; i++)
+                        {
+                            XWPFTableRow xtri = xt.GetRow(tr.Index + i);
+                            XWPFTableCell celli = xtri.GetCell(th.Index);
+                            CT_Tc cttci = celli.GetCTTc();
+                            CT_TcPr tcpri = cttci.AddNewTcPr();
+                            tcpri.AddNewVMerge().val = ST_Merge.@continue;
+                        }
+                    }
+                }
+            }
+            //foreach (TRow tr in table.Thead)
+            //{
+            //    XWPFTableRow xtr = xt.GetRow(tr.Index);
+            //    foreach (TCell th in tr)
+            //    {
+            //        XWPFTableCell cell = xtr.GetCell(th.Index);
+            //        cell.SetText(th.Value);
+            //        if (th.Colspan > 1)
+            //        {
+            //            xtr.MergeCells(th.Index, th.Index + th.Colspan - 1);
+            //            if (th.Rowspan > 1)
+            //            {
+            //                CT_Tc cttc = cell.GetCTTc();
+            //                CT_TcPr tcpr = cttc.AddNewTcPr();
+            //                tcpr.AddNewVMerge().val = ST_Merge.restart;
+            //                for (int i = 1; i < th.Rowspan; i++)
+            //                {
+            //                    XWPFTableRow xtri = xt.GetRow(tr.Index + i);
+            //                    xtri.MergeCells(th.Index, th.Index + th.Colspan - 1);
+            //                    XWPFTableCell celli = xtri.GetCell(th.Index);
+            //                    CT_Tc cttci = celli.GetCTTc();
+            //                    CT_TcPr tcpri = cttci.AddNewTcPr();
+            //                    tcpri.AddNewVMerge().val = ST_Merge.@continue;
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+            #endregion
+            //#region 填充tbody
+            //foreach (TRow tr in table.Tbody)
+            //{
+            //    XWPFTableRow xtr = xt.GetRow(tr.Index + headRowCount);
+            //    foreach (TCell td in tr)
+            //    {
+            //        XWPFTableCell cell = xtr.GetCell(td.Index);
+            //        cell.SetText(td.Value);
+            //        if (td.Colspan > 1)
+            //        {
+            //            xtr.MergeCells(td.Index, td.Index + td.Colspan - 1);
+            //            if (td.Rowspan > 1)
+            //            {
+            //                CT_Tc cttc = cell.GetCTTc();
+            //                CT_TcPr tcpr = cttc.AddNewTcPr();
+            //                tcpr.AddNewVMerge().val = ST_Merge.restart;
+            //                for (int i = 1; i < td.Rowspan; i++)
+            //                {
+            //                    XWPFTableRow xtri = xt.GetRow(tr.Index + headRowCount + i);
+            //                    xtri.MergeCells(td.Index, td.Index + td.Colspan - 1);
+            //                    XWPFTableCell celli = xtri.GetCell(td.Index);
+            //                    CT_Tc cttci = celli.GetCTTc();
+            //                    CT_TcPr tcpri = cttci.AddNewTcPr();
+            //                    tcpri.AddNewVMerge().val = ST_Merge.@continue;
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+            //#endregion
+            //#region 填充tfoot
+            //foreach (TRow tr in table.Tbody)
+            //{
+            //    XWPFTableRow xtr = xt.GetRow(tr.Index + headRowCount + bodyRowCount);
+            //    foreach (TCell td in tr)
+            //    {
+            //        XWPFTableCell cell = xtr.GetCell(td.Index);
+            //        cell.SetText(td.Value);
+            //        if (td.Colspan > 1)
+            //        {
+            //            xtr.MergeCells(td.Index, td.Index + td.Colspan - 1);
+            //            if (td.Rowspan > 1)
+            //            {
+            //                CT_Tc cttc = cell.GetCTTc();
+            //                CT_TcPr tcpr = cttc.AddNewTcPr();
+            //                tcpr.AddNewVMerge().val = ST_Merge.restart;
+            //                for (int i = 1; i < td.Rowspan; i++)
+            //                {
+            //                    XWPFTableRow xtri = xt.GetRow(tr.Index + headRowCount + bodyRowCount + i);
+            //                    xtri.MergeCells(td.Index, td.Index + td.Colspan - 1);
+            //                    XWPFTableCell celli = xtri.GetCell(td.Index);
+            //                    CT_Tc cttci = celli.GetCTTc();
+            //                    CT_TcPr tcpri = cttci.AddNewTcPr();
+            //                    tcpri.AddNewVMerge().val = ST_Merge.@continue;
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+            //#endregion
+            return doc;
+        }
+
+        public static MemoryStream Export(Table table)
+        {
+            XWPFDocument doc = GetDocument(table);
+            MemoryStream ms = new MemoryStream();
+            doc.Write(ms);
+            return ms;
+        }
+
+        public static void Export(Table table, string fileName)
+        {
+            XWPFDocument doc = GetDocument(table);
+            FileStream fs = File.Create(fileName);
+            doc.Write(fs);
+            fs.Close();
+        }
+    }
+}
--- ABC/ABC.Web/Controllers/HomeController.cs
+++ ABC/ABC.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ABC.BLL.NPOI;
+using ABC.BLL.Tabulation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -25,6 +27,142 @@ namespace ABC.Web.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public FileResult ExportDayAQIReport(int count)
+        {
+            Table table = new Table();
+            #region thead
+            TRow tr = table.Thead.AddTr();
+            tr.AddTh(DateTime.Today.ToString("yyyy年MM月dd日"), 1, 21);
+            tr = table.Thead.AddTr();
+            tr.AddTh("城市名称", 4, 1);
+            tr.AddTh("监测点位名称", 4, 1);
+            tr.AddTh("污染物浓度及空气质量分指数（IAQI）", 1, 14);
+            tr.AddTh("空气质量指数（AQI）", 4, 1);
+            tr.AddTh("首要污染物", 4, 1);
+            tr.AddTh("空气质量指数级别", 4, 1);
+            tr.AddTh("空气质量指数类别", 2, 2);
+            tr = table.Thead.AddTr();
+            tr.AddTh("二氧化硫（SO2）24小时平均", 2, 2);
+            tr.AddTh("二氧化氮（NO2）24小时平均", 2, 2);
+            tr.AddTh("颗粒物（粒径小于等于10μm）24小时平均", 2, 2);
+            tr.AddTh("一氧化碳（CO）24小时平均", 2, 2);
+            tr.AddTh("臭氧（O3）最大1小时平均", 2, 2);
+            tr.AddTh("臭氧（O3）最大8小时滑动平均", 2, 2);
+            tr.AddTh("颗粒物（粒径小于等于2.5μm）24小时平均", 2, 2);
+            tr = table.Thead.AddTr();
+            tr.AddTh("类别", 2, 1);
+            tr.AddTh("颜色", 2, 1);
+            tr = table.Thead.AddTr();
+            for (int i = 0; i < 7; i++)
+            {
+                if (i == 3) tr.AddTh("浓度/（mg/m³）");
+                else tr.AddTh("浓度/（μg/m³）");
+                tr.AddTh("分指数");
+            }
+            #endregion
+            #region tfoot
+            tr = table.Tfoot.AddTr();
+            tr.AddTd("注：缺测指标的浓度及分指数均使用NA标识。");
+            #endregion
+            #region tbody
+            Random rand = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                tr = table.Tbody.AddTr();
+                tr.AddTd("广州");
+                tr.AddTd("未知点位");
+                tr.AddTd(rand.Next(100).ToString());
+                tr.AddTd(rand.Next(50).ToString());
+                tr.AddTd(rand.Next(100).ToString());
+                tr.AddTd(rand.Next(50).ToString());
+                tr.AddTd(rand.Next(100).ToString());
+                tr.AddTd(rand.Next(50).ToString());
+                tr.AddTd(rand.Next(10).ToString());
+                tr.AddTd(rand.Next(50).ToString());
+                tr.AddTd(rand.Next(100).ToString());
+                tr.AddTd(rand.Next(50).ToString());
+                tr.AddTd(rand.Next(100).ToString());
+                tr.AddTd(rand.Next(50).ToString());
+                tr.AddTd(rand.Next(100).ToString());
+                tr.AddTd(rand.Next(50).ToString());
+                tr.AddTd(rand.Next(50).ToString());
+                tr.AddTd("NA");
+                tr.AddTd("一级");
+                tr.AddTd("优");
+                tr.AddTd("绿色");
+            }
+            #endregion
+            return File(ExcelHelper.Export(table, 2).ToArray(), "application / vnd.ms - excel", "空气质量指数日报.xlsx");
+        }
+
+        public FileResult ExportDayAQIReportWord(int count)
+        {
+            Table table = new Table();
+            #region thead
+            TRow tr = table.Thead.AddTr();
+            tr.AddTh(DateTime.Today.ToString("yyyy年MM月dd日"), 1, 21);
+            tr = table.Thead.AddTr();
+            tr.AddTh("城市名称", 4, 1);
+            tr.AddTh("监测点位名称", 4, 1);
+            tr.AddTh("污染物浓度及空气质量分指数（IAQI）", 1, 14);
+            tr.AddTh("空气质量指数（AQI）", 4, 1);
+            tr.AddTh("首要污染物", 4, 1);
+            tr.AddTh("空气质量指数级别", 4, 1);
+            tr.AddTh("空气质量指数类别", 2, 2);
+            tr = table.Thead.AddTr();
+            tr.AddTh("二氧化硫（SO2）24小时平均", 2, 2);
+            tr.AddTh("二氧化氮（NO2）24小时平均", 2, 2);
+            tr.AddTh("颗粒物（粒径小于等于10μm）24小时平均", 2, 2);
+            tr.AddTh("一氧化碳（CO）24小时平均", 2, 2);
+            tr.AddTh("臭氧（O3）最大1小时平均", 2, 2);
+            tr.AddTh("臭氧（O3）最大8小时滑动平均", 2, 2);
+            tr.AddTh("颗粒物（粒径小于等于2.5μm）24小时平均", 2, 2);
+            tr = table.Thead.AddTr();
+            tr.AddTh("类别", 2, 1);
+            tr.AddTh("颜色", 2, 1);
+            tr = table.Thead.AddTr();
+            for (int i = 0; i < 7; i++)
+            {
+                if (i == 3) tr.AddTh("浓度/（mg/m³）");
+                else tr.AddTh("浓度/（μg/m³）");
+                tr.AddTh("分指数");
+            }
+            #endregion
+            #region tfoot
+            tr = table.Tfoot.AddTr();
+            tr.AddTd("注：缺测指标的浓度及分指数均使用NA标识。");
+            #endregion
+            #region tbody
+            Random rand = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                tr = table.Tbody.AddTr();
+                tr.AddTd("广州");
+                tr.AddTd("未知点位");
+                tr.AddTd(rand.Next(100).ToString());
+                tr.AddTd(rand.Next(50).ToString());
+                tr.AddTd(rand.Next(100).ToString());
+                tr.AddTd(rand.Next(50).ToString());
+                tr.AddTd(rand.Next(100).ToString());
+                tr.AddTd(rand.Next(50).ToString());
+                tr.AddTd(rand.Next(10).ToString());
+                tr.AddTd(rand.Next(50).ToString());
+                tr.AddTd(rand.Next(100).ToString());
+                tr.AddTd(rand.Next(50).ToString());
+                tr.AddTd(rand.Next(100).ToString());
+                tr.AddTd(rand.Next(50).ToString());
+                tr.AddTd(rand.Next(100).ToString());
+                tr.AddTd(rand.Next(50).ToString());
+                tr.AddTd(rand.Next(50).ToString());
+                tr.AddTd("NA");
+                tr.AddTd("一级");
+                tr.AddTd("优");
+                tr.AddTd("绿色");
+            }
+            #endregion
+            return File(WordHelper.Export(table).ToArray(), "application / vnd.ms - word", "空气质量指数日报.docx");
         }
     }
 }
